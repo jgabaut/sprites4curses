e771a88594162ff8de442d262e30df48a0eef33e
jgabaut <109908086+jgabaut@users.noreply.github.com>
2023-02-25 21:33:56 +0100

    v0.1.4 - Moved demo function inside demo.c (#7)

--- animate.h
+++ animate.h
@@ -9,19 +9,12 @@
 #define BLACK 7
 #define WHITE 8
 
-#define MAXFRAMES 61 /**< Defines the maximum number of sprites.*/
-#define DEMOFRAMES 30 /**< Defines the number of sprites in the demo.*/
+#define MAXFRAMES 121 /**< Defines the maximum number of sprites.*/
 #define MAXROWS 26 /**< Defines the maximum number of rows per sprite.*/
 #define MAXCOLS 84 /**< Defines the maximum number of colums per sprite.*/
-#define DEMOROWS 18 /**< Defines the maximum number of rows per sprite.*/
-#define DEMOCOLS 18 /**< Defines the maximum number of colums per sprite.*/
-
-#define DEMOFRAMETIME 67 /**< Defines for how many millisecs a sprite should stay on screen in the demo.*/
 
 static void print_spriteline(WINDOW* w, char* line, int line_num);
 static char *trim(char *str);
 void load_sprites(char sprites[MAXFRAMES][MAXROWS][MAXCOLS], const char *filename, int rows, int columns);
-void animate_demo_usage(char* progname);
-void init_color_pairs();
+void init_s4c_color_pairs();
 void animate_file(WINDOW* w, char* filename, int repetitions, int frametime, int num_frames, int frameheight, int framewidth);
-void demo(); 
