0707b0e0146a0182dbffccc3823f994811373eea
jgabaut <109908086+jgabaut@users.noreply.github.com>
2024-01-03 02:26:45 +0100

    chore: drop TITLE screen from demo

--- s4c-demo/demo.c
+++ s4c-demo/demo.c
@@ -36,7 +36,7 @@ void usage(char* progname) {
     exit(EXIT_FAILURE);
 }
 
-typedef enum Screen { LOGO = 0, TITLE, GAMEPLAY, ENDING } Screen;
+typedef enum Screen { LOGO = 0, GAMEPLAY, ENDING } Screen;
 
 /*
  * Demo function showing how to call s4c-animate functions correctly.
@@ -468,18 +468,8 @@ int demo(FILE* mainthread_file, FILE* newthread_file) {
                 // TODO: Update LOGO screen variables here!
 
                 framesCounter++;    // Count frames
-                // Wait for logo_sleep frames before jumping to TITLE screen
-                if (framesCounter > logo_sleep)
-                {
-                    currentScreen = TITLE;
-                }
-            } break;
-            case TITLE:
-            {
-                // TODO: Update TITLE screen variables here!
-
-                // Press enter to change to GAMEPLAY screen
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+                // Wait for logo_sleep frames before jumping to GAMEPLAY screen
+                if (framesCounter % logo_sleep == 0)
                 {
                     currentScreen = GAMEPLAY;
                 }
@@ -502,10 +492,10 @@ int demo(FILE* mainthread_file, FILE* newthread_file) {
             case ENDING:
             {
                 // TODO: Update ENDING screen variables here!
-                // Press enter to return to TITLE screen
+                // Press enter to return to LOGO screen
                 if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
                 {
-                    currentScreen = TITLE;
+                    currentScreen = LOGO;
                 }
             } break;
             default: break;
@@ -526,13 +516,6 @@ int demo(FILE* mainthread_file, FILE* newthread_file) {
                     DrawText("LOGO SCREEN", 20, 20, 40, LIGHTGRAY);
                     DrawText("WAIT for 1 SECOND...", 290, 220, 20, GRAY);
                 } break;
-                case TITLE:
-                {
-                    // TODO: Draw TITLE screen here!
-                    DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
-                    DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
-                    DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
-                } break;
                 case GAMEPLAY:
                 {
                     // TODO: Draw GAMEPLAY screen here!
@@ -549,8 +532,7 @@ int demo(FILE* mainthread_file, FILE* newthread_file) {
                     // TODO: Draw ENDING screen here!
                     DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
                     DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
-                    DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
-
+                    DrawText("PRESS ENTER or TAP to RETURN to LOGO SCREEN", 120, 220, 20, DARKBLUE);
                 } break;
                 default: break;
             }
