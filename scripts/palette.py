88be196c58ee24f5680340f86726fc8fa3e1d252
jgabaut <109908086+jgabaut@users.noreply.github.com>
2024-08-09 02:15:56 +0200

    chore: add deprecation warnings to bridge scripts

--- scripts/palette.py
+++ scripts/palette.py
@@ -4,4 +4,8 @@ import sys
 from s4c.core.palette import main
 
 if __name__ == "__main__":
+    print("[WARN]    This bridge script to s4c module is deprecated since 0.4.3, and will be removed in the 0.5 release of the library.")
+    print("[WARN]    To prepare for it, you should either:")
+    print("[WARN]    - Use the s4c module:    \'python -m s4c-scripts.s4c\'")
+    print("[WARN]    - Use a single script directly:    \'python -m s4c-scripts.s4c.core.SCRIPT_NAME\'\n")
     main(sys.argv)
