9680379bc40643b0e9d13b1dd9b2db6faab304a0
jgabaut <109908086+jgabaut@users.noreply.github.com>
2024-08-29 20:06:39 +0200

    chore: guard common demo for raylib build

--- s4c-demo/demo.c
+++ s4c-demo/demo.c
@@ -2,6 +2,8 @@
 
 int main(void) {
     printf("Using s4c-animate v%s\n", string_s4c_version());
+#ifdef S4C_GUI_H_
     printf("Using s4c-gui v%s\n", string_s4c_gui_version());
+#endif // S4C_GUI_H_
     return 0;
 }
